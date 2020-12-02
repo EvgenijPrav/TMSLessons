5fabf405a02045a8d92705dd29c7425e439a6553
Yauheni Pravilau <praviloffevg@gmail.com>
2020-12-02 22:32:32 +0300

    Added source code from lesson 5

---
+++ src/com/company/Lesson5.java
@@ -0,0 +1,52 @@
+package com.company;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Lesson5 {
+    public static void main(String[] args) {
+//        arrays();
+        stuffWithClasses1();
+    }
+
+    static void arrays() {
+        long[][] array;
+        int n;
+        int m;
+        boolean isFromNetwork = false;
+        Scanner scanner = new Scanner(System.in);
+        if (isFromNetwork) {
+            n = getTwo();
+            m = getTwo();
+        } else {
+            System.out.println("Введите размеры массива");
+            n = scanner.nextInt();
+            m = scanner.nextInt();
+        }
+        array = new long[n][m];
+        Random random = new Random();
+        for (int i = 0; i< array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                array[i][j] = scanner.nextLong();
+                System.out.print(array[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    static int getTwo() {
+        return 2;
+    }
+
+    public static void stuffWithClasses1() {
+        Person person1 = new Person();
+        person1.sayName();
+        person1.sayAge();
+        Person person2 = new Person(43, "Adam");
+        person2.age = 76;
+        person2.sayName();
+        person2.sayAge();
+        System.out.println(person1);
+        System.out.println(person2);
+    }
+}
---
+++ src/com/company/Person.java
@@ -0,0 +1,50 @@
+package com.company;
+
+import java.util.Random;
+
+public class Person {
+
+    int age;
+    String name;
+
+    public Person() {
+        this(25, "Mary");
+        System.out.println("Constructor without arguments has been called");
+    }
+
+    public Person(int age, String name) {
+        System.out.println("Constructor with 2 arguments has been called");
+        this.age = age;
+        this.name = name;
+    }
+
+    public Person(int age) {
+        this(age, "Mary");
+    }
+
+    public Person(String name) {
+        age = 30;
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+
+    public void sayAge() {
+        System.out.println("My age is " + age);
+    }
+
+    public void sayName() {
+        System.out.println("My name is " + name);
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "age=" + age +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
